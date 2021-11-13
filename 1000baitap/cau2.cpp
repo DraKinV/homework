1abffd9d5db50cad921213ed4863c5d5d5586f18
DraKinV <tien_dth216188@student.agu.edu.vn>
2021-11-13 10:35:34 +0700

    homework lan 1

---
+++ 1000baitap/cau2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+	int n,S;
+	printf ("Nhap n: ");
+	scanf ("%d",&n);
+	for (int i=1; i<=n;i++) 
+		S+=i*i;
+	printf ("S(n) = %d",S);
+	return 0;
+}
---
+++ 1000baitap/cau3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+	int n;
+	float S;
+	printf ("Nhap n: ");
+	scanf ("%d",&n);
+	for (int i=1; i<=n;i++) 
+		S+=(float) 1/i;
+	printf ("S(n) = %.2f",S);
+	return 0;
+}
